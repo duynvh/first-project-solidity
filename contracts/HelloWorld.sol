e3f0e0e1a7b57d9fa4469812f9bdac3fb73f37b8
duynvh <nguyenvohoangduy@gmail.com>
2022-03-21 22:36:45 +0700

    Chore: Import hardhat/console.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,17 +1,19 @@
 pragma solidity ^0.8.4;
+import "hardhat/console.sol";
 
 contract HelloWorld {
-	string public message;
+    string public message;
 
-	constructor(string memory _message) {
-		message = _message;
-	}
+    constructor(string memory _message) {
+        message = _message;
+    }
 
-	function print() public view returns (string memory) {
-		return message;
-	}
+    function print() public view returns (string memory) {
+        return message;
+    }
 
-	function update(string memory _message) public {
-		message = _message;
-	}
+    function update(string memory _message) public {
+        console.log("message:", _message);
+        message = _message;
+    }
 }
